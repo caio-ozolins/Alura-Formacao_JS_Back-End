363cdff358d1e6855d435408c365cc56d576a16c
Caio Ozolins Mendes <caio.ozolins@gmail.com>
2023-03-29 14:05:21 -0300

    01. Tipos Primitivos - 06 Tipo string

---
+++ Tipos, Variáveis e Funções/01. Tipos Primitivos/06 Tipo string/type-string.js
@@ -0,0 +1,15 @@
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+const senha = "senhaSuperSegura456!";
+const StringDeNumeros = "34567";
+
+const citacao = 'Meu nome é ';
+const nome = "Caio";
+
+console.log(citacao)
+
+// concatenação (+)
+
+console.log(citacao+nome)
+
+// template string OU template literal
