b6f662fefec04f12c236c4b39852c6ae55717f15
Caio Ozolins Mendes <caio.ozolins@gmail.com>
2023-03-29 14:04:13 -0300

    01. Tipos Primitivos - 04 Tipo number

---
+++ Tipos, Variáveis e Funções/01. Tipos Primitivos/04 Tipo number/type-number.js
@@ -0,0 +1,23 @@
+// tipo Number
+
+const num1 = 1;
+const num2 = 2;
+
+const operation = num1 - num2;
+
+console.log(operation)
+
+// ponto flutuante
+
+const floatnum1 = 3.3;
+const floatnum2 = .5;
+
+const operation2 = num1 / floatnum1;
+
+console.log(operation2)
+
+// NaN -> Not a Number
+
+const x = "Alura";
+
+console.log(x * num1)
