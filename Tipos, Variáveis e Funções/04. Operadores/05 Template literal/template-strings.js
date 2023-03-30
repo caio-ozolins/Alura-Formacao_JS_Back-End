2a634ab7f2e05207cb41235512d1a926116decab
Caio Ozolins Mendes <caio.ozolins@gmail.com>
2023-03-30 14:07:43 -0300

    04. Operadores - 05 Template literal

---
+++ Tipos, Variáveis e Funções/04. Operadores/05 Template literal/template-strings.js
@@ -0,0 +1,9 @@
+const nome = 'Caio'
+const idade = 2023 - 2002
+const cidadeNascimento = 'São Paulo'
+
+// const apresentacao = 'Meu nome é ' + nome + ', minha idade é ' + idade + ' e nasci na cidade de ' + cidadeNascimento
+
+const apresentacao = `Meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidadeNascimento}`
+
+console.log(apresentacao)
