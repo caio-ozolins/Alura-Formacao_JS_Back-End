94573d4cc8e17ebf2fc049dc87e4337ab5855012
Caio Ozolins Mendes <caio.ozolins@gmail.com>
2023-03-30 09:42:59 -0300

    02. Declarando Variáveis - 03 Truthy e falsy

---
+++ Tipos, Variáveis e Funções/02. Declarando Variáveis/03 Truthy e falsy/truthy-falsy.js
@@ -0,0 +1,34 @@
+// boolean
+const usuarioLogado = true
+const contapaga = false
+
+// truthy e falsy
+
+// 0 -> false
+// 1 -> true
+
+// "" [string vazia] -> false
+
+/*
+console.log(0 == false)
+console.log("" == false)
+console.log(1 == true)
+ */
+
+// null -> vazio ou nada
+
+let minhaVar;
+let varNull = null;
+
+/*
+console.log(minhaVar)
+console.log(varNull)
+ */
+
+let num = 3;
+let text = 'alura';
+
+console.log(typeof num)
+console.log(typeof text)
+console.log(typeof minhaVar)
+console.log(typeof varNull)
