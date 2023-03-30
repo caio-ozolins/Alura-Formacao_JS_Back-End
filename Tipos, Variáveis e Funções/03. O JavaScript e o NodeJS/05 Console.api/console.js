bee5d31dbb109b55b8cb5bd673eebfb2962e43b6
Caio Ozolins Mendes <caio.ozolins@gmail.com>
2023-03-30 11:20:35 -0300

    03. O JavaScript e o NodeJS - 05 Console.api

---
+++ Tipos, Variáveis e Funções/03. O JavaScript e o NodeJS/05 Console.api/console.js
@@ -0,0 +1,15 @@
+// log -> registro
+
+console.time()
+
+const minhaVar = true;
+
+console.log(123)
+console.log('texto')
+console.log(minhaVar)
+
+// tratamento de erro
+
+console.error('Deu bosta!')
+
+console.timeEnd()
