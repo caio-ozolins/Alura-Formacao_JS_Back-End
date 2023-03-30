eeba5e8db244f06999c16f39b321d861985c3e2b
Caio Ozolins Mendes <caio.ozolins@gmail.com>
2023-03-30 10:19:47 -0300

    02. Declarando Variáveis - 04 Conversão de tipos

---
+++ Tipos, Variáveis e Funções/02. Declarando Variáveis/04 Conversão de tipos/conversoes.js
@@ -0,0 +1,20 @@
+// tipo de dado
+// booleans
+
+// conversão implícita
+
+const num = 456;
+const numStr = '456';
+
+console.log(num === numStr)
+console.log(num == numStr)
+
+console.log(num + numStr)
+
+// Number()
+// String()
+
+console.log(num + Number(numStr))
+console.log(Number('456a'))
+
+// conversão explicita
